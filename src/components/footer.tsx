68eedbca830473027d620027b4154a4066c56b7f
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-09-30 20:24:20 +0300

    Add Footer component

---
+++ src/components/footer.tsx
@@ -0,0 +1,87 @@
+import React from 'react'
+import { FaFacebook, FaTwitter, FaLinkedin, FaInstagram } from 'react-icons/fa'
+
+export function Footer() {
+    return (
+        <footer className='flex h-full flex-grow flex-col bg-white py-4 shadow-md'>
+            {/* Upper section */}
+            <div className='mx-4 flex h-32 flex-col items-center justify-between md:mx-20 md:flex-row'>
+                <div className='flex items-center space-x-4'>
+                    <p className='text-3xl font-bold'>Sentlytics</p>
+                    <div className='flex flex-col space-y-1'>
+                        <p className='text-left'>
+                            Address: 123 Main St, Anytown, USA
+                        </p>
+                        <p className='text-left'>All Rights Are Reserved</p>
+                    </div>
+                </div>
+                <select className='my-2 mt-4 rounded-md border bg-white md:mt-0'>
+                    <option value='en'>English</option>
+                    <option value='es'>Spanish</option>
+                    <option value='fr'>French</option>
+                </select>
+            </div>
+
+            {/* Lower section */}
+            <div className='mx-4 flex h-full flex-col items-center justify-between border-t pt-4 md:mx-12 md:flex-row'>
+                <div className='flex flex-col space-y-2 md:flex-row md:space-x-4 md:space-y-0'>
+                    <a
+                        href='/terms'
+                        className='border-l-2 pl-2 text-gray-600 md:border-l-0 md:pl-0'
+                    >
+                        Terms and Conditions
+                    </a>
+                    <a
+                        href='/privacy'
+                        className='border-l-2 pl-2 text-gray-600 md:border-l-0 md:pl-0'
+                    >
+                        Privacy Policy
+                    </a>
+                    <a
+                        href='/security'
+                        className='border-l-2 pl-2 text-gray-600 md:border-l-0 md:pl-0'
+                    >
+                        Security
+                    </a>
+                    <a
+                        href='/accessibility-statement'
+                        className='border-l-2 pl-2 text-gray-600 md:border-l-0 md:pl-0'
+                    >
+                        Accessibility Statement
+                    </a>
+                </div>
+
+                <div className='mt-2 flex space-x-4 md:mt-0'>
+                    <a
+                        href='https://www.facebook.com'
+                        target='_blank'
+                        rel='noopener noreferrer'
+                    >
+                        <FaFacebook className='text-gray-600 hover:text-blue-600' />
+                    </a>
+                    <a
+                        href='https://twitter.com'
+                        target='_blank'
+                        rel='noopener noreferrer'
+                    >
+                        <FaTwitter className='text-gray-600 hover:text-blue-400' />
+                    </a>
+                    <a
+                        href='https://www.linkedin.com'
+                        target='_blank'
+                        rel='noopener noreferrer'
+                    >
+                        <FaLinkedin className='text-gray-600 hover:text-blue-700' />
+                    </a>
+                    <a
+                        href='https://www.instagram.com'
+                        target='_blank'
+                        rel='noopener noreferrer'
+                    >
+                        <FaInstagram className='text-gray-600 hover:text-pink-500' />
+                    </a>
+                </div>
+            </div>
+        </footer>
+    )
+}

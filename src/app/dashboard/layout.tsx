8f7610913599dc6d4840e9010e026e85783027a4
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-10-03 02:42:34 +0300

    Refactor layout component and improve sidebar placement

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,22 +2,24 @@ import SideBar from '@/components/dashboard/side-bar'
 // import { getServerAuthSession } from '@/server/auth'
 // import { redirect } from 'next/navigation'
 
-export default async function HRDashboardPage({
+export default async function Layout({
     children,
-}: Readonly<{
+}: {
     children: React.ReactNode
-}>) {
-    // const session = await getServerAuthSession()
+}) {
+    // Uncomment this section to enable authentication
+    // const session = await getServerAuthSession();
     // if (!session) {
-    //     const callbackURL = encodeURIComponent('/dashboard') // Replace with the current URL if needed
-    //     redirect(`/signin?callbackURL=${callbackURL}`)
+    //     const callbackURL = encodeURIComponent('/dashboard'); // Redirect to sign-in with callback URL
+    //     redirect(`/signin?callbackURL=${callbackURL}`);
     // }
+
     return (
-        <div className='container flex min-h-screen-no-nav py-6'>
-            <div className='hidden w-[30%] xl:block'>
-                <SideBar />
-            </div>
-            <div className='w-full'>{children}</div>
+        <div className='flex min-h-screen py-6'>
+            <SideBar />
+            <main className='mx-auto mb-12 w-full pr-6 md:mb-0 md:w-[70%]'>
+                {children}
+            </main>
         </div>
     )
 }

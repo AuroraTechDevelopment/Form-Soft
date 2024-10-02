142038d605a6209b82b61c9def2fe528d95dddbe
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-10-03 02:42:50 +0300

    Added feedback route

---
+++ src/app/api/feedback/route.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from 'next/server'
+import prisma from '@/server/prisma'
+
+// POST method to handle feedback submission
+export async function POST(req: Request) {
+    try {
+        const { category, comment, userID } = await req.json()
+
+        // Create feedback entry in the database using Prisma
+        const feedback = await prisma.feedback.create({
+            data: {
+                category,
+                comment,
+                userID,
+                createdAt: new Date(), // Consider Adding --> createdAt DateTime @default(now()) in schema.prisma
+            },
+        })
+
+        return NextResponse.json(
+            { message: 'Feedback submitted successfully', feedback },
+            { status: 200 },
+        )
+    } catch (error) {
+        console.error('Error submitting feedback:', error)
+        return NextResponse.json(
+            { error: 'Failed to submit feedback' },
+            { status: 500 },
+        )
+    }
+}

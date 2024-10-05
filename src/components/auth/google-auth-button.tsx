8e350f33281ebf0ba7dee3198a9d4ed1f2d4c032
Ahmed Tarek <ahmedtarekabd2002@gmail.com>
2024-10-05 17:41:14 +0300

    Add Google Provider.

--- src/components/auth/google-auth-button.tsx
+++ src/components/auth/google-auth-button.tsx
@@ -1,16 +1,22 @@
 import googleIcon from '@/public/icons/google-icon.png'
 import Image from 'next/image'
 import { Button } from '../ui/button'
-import { signIn } from 'next-auth/react'
+import { createClient } from '@/lib/supabase/client'
 
 // Google icon source: https://www.iconfinder.com/icons/7123025/logo_google_g_icon
 const GoogleAuthButton = () => {
+    const supabase = createClient()
     return (
         <Button
             variant={'outline'}
             className='flex w-full items-center justify-center gap-2'
-            onClick={() => {
-                signIn('google', { callbackUrl: '/' })
+            onClick={async () => {
+                await supabase.auth.signInWithOAuth({
+                    provider: 'google',
+                    options: {
+                        redirectTo: `${process.env.NEXT_PUBLIC_OAUTH_URL}/api/auth/callback`,
+                    },
+                })
             }}
         >
             <Image src={googleIcon} alt='Google icon' width={30} height={30} />

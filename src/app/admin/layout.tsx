c536d39d0f44f8ae0e69c3f54ec8a3862a1d19ca
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-10-09 18:00:03 +0300

    Added admin dashboard layout component

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,25 @@
+import SideBar from '@/components/dashboard/side-bar'
+// import { getServerAuthSession } from '@/server/auth'
+// import { redirect } from 'next/navigation'
+
+export default async function Layout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    // Uncomment this section to enable authentication
+    // const session = await getServerAuthSession();
+    // if (!session) {
+    //     const callbackURL = encodeURIComponent('/dashboard'); // Redirect to sign-in with callback URL
+    //     redirect(`/signin?callbackURL=${callbackURL}`);
+    // }
+
+    return (
+        <div className='flex min-h-screen py-6'>
+            <SideBar type='admin' />
+            <main className='mx-auto mb-12 w-full pr-6 md:mb-0 md:w-[70%]'>
+                {children}
+            </main>
+        </div>
+    )
+}

194fac5f8bf32f4cb504a018e99df408aeb17d81
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-11-15 03:21:20 +0200

    Add UserContext and UserProvider for managing user state

---
+++ src/context/UserContext.tsx
@@ -0,0 +1,28 @@
+'use client'
+import { createContext, useContext } from 'react'
+
+interface UserContextType {
+    user: any | null
+}
+
+const UserContext = createContext<UserContextType | undefined>(undefined)
+
+export const UserProvider: React.FC<{
+    user: any
+    children: React.ReactNode
+}> = ({ user, children }) => {
+    return (
+        <UserContext.Provider value={{ user }}>{children}</UserContext.Provider>
+    )
+}
+
+export const useUser = () => {
+    const context = useContext(UserContext)
+    if (!context) {
+        console.error(
+            'UserContext is not available. Did you forget to wrap your component with UserProvider?',
+        )
+        throw new Error('useUser must be used within a UserProvider')
+    }
+    return context
+}

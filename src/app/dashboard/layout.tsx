e99073c495dbcdedbae7b3681f0c5866ab79bfa5
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-11-15 03:19:52 +0200

    Enhance dashboard layout: Implement user authentication check and wrap layout in UserProvider

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,25 +1,35 @@
 import SideBar from '@/components/dashboard/side-bar'
-// import { getServerAuthSession } from '@/server/auth'
-// import { redirect } from 'next/navigation'
+import { redirect } from 'next/navigation'
+import { createClient } from '@/lib/supabase/server'
+import UserNavbar from '@/components/dashboard/user-navbar'
+import { UserProvider } from '@/context/UserContext'
 
 export default async function Layout({
     children,
 }: {
     children: React.ReactNode
 }) {
-    // Uncomment this section to enable authentication
-    // const session = await getServerAuthSession();
-    // if (!session) {
-    //     const callbackURL = encodeURIComponent('/dashboard'); // Redirect to sign-in with callback URL
-    //     redirect(`/signin?callbackURL=${callbackURL}`);
-    // }
+    const supabase = createClient()
+    const {
+        data: { user },
+        error,
+    } = await supabase.auth.getUser()
+
+    if (error || !user) {
+        redirect('/')
+    }
 
     return (
-        <div className='flex min-h-screen py-6'>
-            <SideBar type='user' />
-            <main className='mx-auto mb-12 w-full pr-6 md:mb-0 md:w-[70%]'>
-                {children}
-            </main>
-        </div>
+        <UserProvider user={user}>
+            <div className='min-h-screen flex-col'>
+                <UserNavbar />
+                <div className='flex min-h-screen py-6'>
+                    <SideBar type='user' />
+                    <main className='mx-auto mb-12 w-full pr-6 md:mb-0 md:w-[70%]'>
+                        {children}
+                    </main>
+                </div>
+            </div>
+        </UserProvider>
     )
 }
